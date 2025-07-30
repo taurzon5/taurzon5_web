198271bbaf5a7c2daf9262ac9e5045254691283f
Khrystan Taur <73740076+taurzon5@users.noreply.github.com>
2025-07-30 03:47:03 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 const nextConfig = {
   output: 'export',
   reactStrictMode: true,
-  basePath: process.env.NODE_ENV === 'production' ? '/taurzon5_web' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/taurzon5_web/' : '',
+  basePath: '/taurzon5_web',
+  assetPrefix: '/taurzon5_web/',
   images: {
     unoptimized: true,
   },
