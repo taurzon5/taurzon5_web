0a415816e1ad3d885e747d335522be25e5378f8f
Khrystan Taur <73740076+taurzon5@users.noreply.github.com>
2025-07-30 02:31:28 -0400

    Update next.config.ts 2

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
   basePath: "/taurzon5_web",
   output: "export",
   reactStrictMode: true,
