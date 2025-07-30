073022a89d06e8e50e5d9d9eceb2d7bd3ccee2ab
Khrystan Taur <73740076+taurzon5@users.noreply.github.com>
2025-07-30 03:46:24 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,26 +7,6 @@ const nextConfig = {
     unoptimized: true,
   },
   trailingSlash: true,
-  webpack: (
-    config,
-    {
-      buildId,
-      dev,
-      isForcibleStaticBuild,
-      isServer,
-      nextRuntime,
-      webpack,
-    }
-  ) => {
-    config.module.rules.push({
-      test: /\.pdf$/,
-      type: 'asset/resource',
-      generator: {
-        filename: 'static/media/[name].[hash][ext]',
-      },
-    });
-    return config;
-  },
 };
 
 module.exports = nextConfig;
