6993040658ced127eaeaf489e28450537556fa6b
Khrystan Taur <73740076+taurzon5@users.noreply.github.com>
2025-07-30 03:33:17 -0400

    Update next.config.ts 7

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,18 @@ const nextConfig = {
   trailingSlash: true, // Often helpful for static exports
   output: "export",
   reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.pdf$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+        },
+      },
+    });
+    return config;
+  }
 };
 
 module.exports = nextConfig;
