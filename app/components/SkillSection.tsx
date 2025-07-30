ccd49694ad4047f3e88a2d37035c36b7f5037fe1
Khrystan Taur <73740076+taurzon5@users.noreply.github.com>
2025-07-30 03:09:22 -0400

    Update SkillSection.tsx

--- app/components/SkillSection.tsx
+++ app/components/SkillSection.tsx
@@ -7,7 +7,7 @@ export default function HeroSection() {
 		<section className="relative min-h-screen flex items-center justify-center overflow-hidden">
 			<div className="absolute inset-0 bg-gradient-to-r from-green-600/20 to-blue-600/20 opacity-10" />
 			<div className="absolute inset-0">
-				<div className="absolute inset-0 bg-[url('taurzon5_web/grid.svg')] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]" />
+				<div className="absolute inset-0 bg-[url('/taurzon5_web/grid.svg')] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]" />
 			</div>
 
 			<div className="relative z-10 max-w-4xl w-full mx-4">
